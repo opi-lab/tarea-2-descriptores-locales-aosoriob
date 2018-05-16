2cb01b58195b94a138ee741410c56694779da606
aosoriob <36336817+aosoriob@users.noreply.github.com>
2018-05-16 15:23:40 -0500

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 21:27:19 2018
+
+@author: alexanderosorio
+"""
+
+from numpy import *
+import harris
+im1 = array(Image.open('harrys1.jpg').convert('L')) 
+im2 = array(Image.open('harrys2.jpg').convert('L')) 
+wid = 5
+harrisim = harris.compute_harris_response(im1,5)
+
+filtered_coords1 = harris.get_harris_points(harrisim,wid+1) 
+d1 = harris.get_descriptors(im1,filtered_coords1,wid)
+harrisim = harris.compute_harris_response(im2,5) 
+filtered_coords2 = harris.get_harris_points(harrisim,wid+1) 
+d2 = harris.get_descriptors(im2,filtered_coords2,wid)
+print ('starting matching')
+matches = harris.match_twosided(d1,d2)
+figure()
+gray() 
+harris.plot_matches(im1,im2,filtered_coords1,filtered_coords2,matches) 
+show()
